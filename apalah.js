1032be4eff8fa9656b1d7b3deabed4ed988ccab4
jekmts456-pixel <jekmts456@gmail.com>
2026-07-11 21:58:55 +0700

    upload project

---
+++ apalah.js
@@ -0,0 +1,55 @@
+document.getElementById("formDaftar").addEventListener("submit", function (event) {
+    event.preventDefault();
+  const tombol = document.querySelector("button");
+tombol.disabled = true;
+tombol.textContent = "Mengirim...";
+
+    const formData = new FormData(event.target);if (data.hp.length < 10) {
+    alert("Nomor HP minimal 10 digit!");
+
+    tombol.disabled = false;
+    tombol.textContent = "KIRIM";
+
+    return;
+}
+    const data = Object.fromEntries(formData.entries()); 
+}
+
+    const hasilTeks = `
+Nama: ${data.nama}
+Kelas: ${data.kelas}
+JK: ${data.jk}
+Tgl Lahir: ${data.tgl_lahir}
+HP: ${data.hp}
+Email: ${data.email}
+Alamat: ${data.alamat}
+Pesan: ${data.pesan}
+`;
+
+    document.getElementById("output").textContent = hasilTeks;
+    document.getElementById("hasilData").style.display = "block";
+
+    fetch("https://script.google.com/macros/s/AKfycby3ufXtLeO404huKrkbipgDN8FC6VQRVTZkFrQ858lUe6N-cwoEcY1KZMzIgfUI83H0lg/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(() => {
+        alert("Data berhasil dikirim!");
+        
+    })
+    
+    .catch((error) => {
+    console.log(error);
+
+    tombol.disabled = false;
+    tombol.textContent = "KIRIM";
+
+    alert("Gagal mengirim data.");
+})
+
+event.target.reset();
+});
