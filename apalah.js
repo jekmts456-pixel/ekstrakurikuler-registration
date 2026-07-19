e4ffc75b183111a6953737ea14a8593ff6d2f97d
jekmts456-pixel <jekmts456@gmail.com>
2026-07-19 20:51:13 +0700

    Update apalah.js
    change all

--- apalah.js
+++ apalah.js
@@ -1,58 +1,38 @@
 document.getElementById("formDaftar").addEventListener("submit", function (event) {
-
     event.preventDefault();
 
     const tombol = document.querySelector("button");
-
     tombol.disabled = true;
     tombol.textContent = "Mengirim...";
 
     const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData.entries());
 
-    if (data.hp.length < 10) {
-
+    if (formData.get("hp").length < 10) {
         alert("Nomor HP minimal 10 digit!");
-
         tombol.disabled = false;
         tombol.textContent = "KIRIM";
-
         return;
     }
 
-    const hasilTeks = `
-Nama: ${data.nama}
-Kelas: ${data.kelas}
-JK: ${data.jk}
-Tgl Lahir: ${data.tgl_lahir}
-HP: ${data.hp}
-Email: ${data.email}
-Alamat: ${data.alamat}
-Pesan: ${data.pesan}
-`;
-
-    document.getElementById("output").textContent = hasilTeks;
-    document.getElementById("hasilData").style.display = "block";
-
     fetch("https://script.google.com/macros/s/AKfycbxOnxtJSZ3F_CSAG56mRXPt9BokdAY4xuuN0Ad-V3CiMZVPNwTRYMqL0EPwgBuW9Gtfig/exec", {
         method: "POST",
-        body: JSON.stringify(data)
+        body: new URLSearchParams(formData)
     })
-
-    .then(() => {
-
-        alert("Data berhasil dikirim!");
-
+    .then(response => response.text())
+    .then(data => {
+        alert("Pendaftaran berhasil. Terima kasih.");
         event.target.reset();
+
+        document.getElementById("hasilData").style.display = "none";
 
         tombol.disabled = false;
         tombol.textContent = "KIRIM";
-
     })
+    .catch(error => {
+        console.error(error);
+        alert("Gagal mengirim data.");
 
-    .catch((error) => {
-    console.error(error);
-    alert(error.message);
-});
-    
+        tombol.disabled = false;
+        tombol.textContent = "KIRIM";
+    });
 });
